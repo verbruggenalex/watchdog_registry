92e273fa2afb85c75d358db6b9b4d047bebce068
Alex Verbruggen <verbruggenalex@gmail.com>
2020-12-05 10:27:05 +0000

    Screenshots.

---
+++ tests/Behat/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WatchdogRegistry\Behat;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Defines step definitions that are generally useful for the project.
+ */
+class FeatureContext extends RawDrupalContext {
+
+
+}
